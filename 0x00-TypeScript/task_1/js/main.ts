2919c02a1afe1f7b234ae1ef957812a4d9f9a293
andreshugueth <andreshugueth@gmail.com>
2021-10-25 23:47:10 -0500

    0x00 TS task 1-2

---
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -0,0 +1,32 @@
+// Task 1
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [attribute: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+console.log(teacher3);
+
+// Task 2
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
