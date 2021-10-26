a643bd193e60528164563d3173420ad269b922ed
andreshugueth <andreshugueth@gmail.com>
2021-10-26 14:39:19 -0500

    0x00 TS task 6-7

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -1,3 +1,4 @@
+// Task 5
 interface DirectorTeacherCommon {
   workFromHome: () => string;
   getCoffeeBreak: () => string;
@@ -43,11 +44,39 @@ class Teacher implements TeacherInterface {
 const createEmployee = (salary: number | string): Director | Teacher => {
   if (typeof salary === "number") {
     if (salary < 500) {
-      return new Teacher();
+      return new Teacher;
     }
   }
-  return new Director();
+  return new Director;
 }
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
+
+// Task 6
+function isDirector(employee: Director | Teacher) {
+  if ("workDirectorTasks" in employee) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+function executeWork(employee: Director | Teacher) {
+  isDirector(employee);
+}
+
+executeWork(createEmployee(200));
+executeWork(createEmployee(1000));
+
+type Subjects = "Math" | "History";
+
+const teachClass = (todayClass: Subjects): string => {
+  if (todayClass === "Math") {
+    return "Teaching Math"
+  } else {
+    return "Teaching History"
+  }
+}
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
