c2ab116c2d7c09cebcae2f7e09f50d8a8e224ca1
andreshugueth <andreshugueth@gmail.com>
2021-10-28 23:49:49 -0500

    0x01 webpack task 2 fix loaders

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -1,34 +1,34 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "production",
-  entry: "./js/dashboard_main.js",
+  entry: './js/dashboard_main.js',
+  mode: 'production',
   performance: {
-    maxAssetSize: 1000000,
+    maxAssetSize: 1000000
   },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "public")
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(gif|png|jpe?g|svg)$/i,
+        test: /\.(gif|png|svg|jpe?g)$/i,
         use: [
-          "file-loader",
+          'file-loader',
           {
-            loader: "image-webpack-loader",
+            loader: 'image-webpack-loader',
             options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
-            },
-          },
-        ],
-      },
-    ],
-  },
+              bypassOnDebug: true,
+              disable: true
+            }
+          }
+        ]
+      }
+    ]
+  }
 };
