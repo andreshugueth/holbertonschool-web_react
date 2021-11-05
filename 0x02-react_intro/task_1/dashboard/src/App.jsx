ba55c0de47d035536ae17a71edc2035e1cd9fa0e
andreshugueth <andreshugueth@gmail.com>
2021-11-04 21:37:30 -0500

    0x02 react intro task 1

---
+++ 0x02-react_intro/task_1/dashboard/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import HolbertonLogo from './HolbertonLogo.jpg';
+import { getFooterCopy, getFullYear } from './utils';
+
+function App() {
+  return (
+    <>
+      <div className="App-header">
+        <img src={HolbertonLogo} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
