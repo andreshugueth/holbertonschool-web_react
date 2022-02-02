f1af69fb60674cae8f1bf55a10e15c9c3b14c563
andreshugueth <andreshugueth@gmail.com>
2022-02-02 04:34:49 -0500

    0x01 task 3 test fix check errors

--- 0x01-Webpack/task_3/modules/body/body.js
+++ 0x01-Webpack/task_3/modules/body/body.js
@@ -1,22 +1,26 @@
-const $ = require("jquery");
-const _ = require("lodash");
-import "./body.css";
+import $ from 'jquery';
+const _ = require('lodash');
+import './body.css';
 
-let count = 0;
+$('body').append('<p id="count"></p>');
+$('body').append('<button id="click-here">Click here to get started</button>');
+
+const counter = (function () {
+  let counter = 0;
+
+  return function () {
+    counter++;
+    return counter;
+  };
+})();
 
 function updateCounter() {
-  count++;
-  $("#count").text(`${count} clicks on the button`);
+  $('#count').text(counter() + ' clicks on the button');
 }
 
-const $button = $("<button>Click here to get started</button>").on(
-  "click",
-  _.debounce(updateCounter, 500, {
-    leading: true,
-    trailing: false,
-  })
-);
+$('#click-here').on('click', _.debounce(updateCounter, 300, {
+  leading: true,
+  trailing: false,
+}));
 
-$("body").append("<p>Dashboard data for the students</p>");
-$("body").append($button);
-$("body").append("<p id='count'></p>");
+// 500
