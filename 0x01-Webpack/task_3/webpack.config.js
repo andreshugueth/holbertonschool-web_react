ecbe69c19d20d0b2b99ad0c01973241abc02c1d9
andreshugueth <andreshugueth@gmail.com>
2022-02-02 04:30:38 -0500

    0x01 webpack task 3

---
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -0,0 +1,55 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const rulesForCss = {
+  test: /\.css$/i,
+  use: ['style-loader', 'css-loader']
+}
+
+const rulesForImages = {
+  test: /\.(gif|png|svg|jpe?g)$/i,
+  use: [
+    'file-loader',
+    {
+      loader: 'image-webpack-loader',
+      options: {
+        bypassOnDebug: true,
+        disable: true
+      }
+    }
+  ]
+}
+
+const rules = [rulesForCss, rulesForImages];
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin()],
+  performance: {
+    hints: false,
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
+  },
+  devServer: {
+    compress: true,
+    open: true,
+    port: 8564,
+  },
+  module: { rules }
+};
